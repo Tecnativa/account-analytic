5ec2b74db0ce9f085e66de76ac0f0026d493745d
Víctor Martínez <victor.martinez@tecnativa.com>
2023-10-19 17:02:27 +0200

    [ADD] project_analytic_tag: New module
    TT42272

---
+++ project_analytic_tag/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Analytic Tag",
+    "version": "16.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/account-analytic",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["project", "account_analytic_tag"],
+    "installable": True,
+    "auto_install": True,
+    "data": [
+        "views/project_task_view.xml",
+    ],
+    "maintainers": ["victoralmau"],
+}
